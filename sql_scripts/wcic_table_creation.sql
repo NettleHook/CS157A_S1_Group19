8889f9dd4ffd3b7c047ff1601da4388c7fb652f2
Alexander Chang <alexanderchang140@gmail.com>
2026-03-17 19:12:18 -0700

    style: use standard sql convention for table names and attributes

--- sql_scripts/wcic_table_creation.sql
+++ sql_scripts/wcic_table_creation.sql
@@ -1,91 +1,91 @@
-CREATE TABLE Guests (
-    sessionID VARCHAR(50) PRIMARY KEY
+CREATE TABLE guests (
+    session_id VARCHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE Users (
-    userID VARCHAR(50) PRIMARY KEY,
+CREATE TABLE users (
+    user_id VARCHAR(50) PRIMARY KEY,
     password VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Ingredients (
+CREATE TABLE ingredients (
     name VARCHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE RecipeSummary (
+CREATE TABLE recipe_summaries (
     name VARCHAR(50) PRIMARY KEY,
-    ServingSize VARCHAR(50),
-    prepTime INT,
-    cookTime INT,
+    serving_size VARCHAR(50),
+    prep_time_min INT,
+    cook_time_min INT,
     calories INT
 );
 
-CREATE TABLE FullRecipe (
-    recipeName VARCHAR(50) PRIMARY KEY,
-    IngredientList VARCHAR(1000)  NOT NULL,
-    Steps VARCHAR(10000)  NOT NULL
+CREATE TABLE recipe_full (
+    recipe_name VARCHAR(50) PRIMARY KEY,
+    ingredient_list VARCHAR(1000)  NOT NULL,
+    steps VARCHAR(10000)  NOT NULL
 );
 
-CREATE TABLE Category (
+CREATE TABLE categories (
     name VARCHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE Diet (
+CREATE TABLE diets (
     name VARCHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE RecipeCategory (
-    recipeName VARCHAR(50),
-    categoryName VARCHAR(50),
-    PRIMARY KEY (recipeName , categoryName)
+CREATE TABLE recipe_categories (
+    recipe_name VARCHAR(50),
+    category_name VARCHAR(50),
+    PRIMARY KEY (recipe_name , category_name)
 );
 
-CREATE TABLE RecipeDiet (
-    recipeName VARCHAR(50),
-    dietName VARCHAR(50),
-    PRIMARY KEY (recipeName , dietName)
+CREATE TABLE recipe_diets (
+    recipe_name VARCHAR(50),
+    diet_name VARCHAR(50),
+    PRIMARY KEY (recipe_name , diet_name)
 );
 
-CREATE TABLE RecipeIngredients (
-    recipeName VARCHAR(50),
-    ingredientName VARCHAR(50),
+CREATE TABLE recipe_ingredients (
+    recipe_name VARCHAR(50),
+    ingredient_name VARCHAR(50),
     amounts VARCHAR(50)  NOT NULL,
-    PRIMARY KEY (recipeName , ingredientName)
+    PRIMARY KEY (recipe_name , ingredient_name)
 );
 
-CREATE TABLE LikedRecipes (
-    userId VARCHAR(50),
-    recipeName VARCHAR(50),
-    PRIMARY KEY (userId , recipeName)
+CREATE TABLE liked_recipes (
+    user_id VARCHAR(50),
+    recipe_name VARCHAR(50),
+    PRIMARY KEY (user_id , recipe_name)
 );
 
-CREATE TABLE BookmarkedRecipes (
-    userId VARCHAR(50),
-    recipeName VARCHAR(50),
-    PRIMARY KEY (userId , recipeName)
+CREATE TABLE bookmarked_recipes (
+    user_id VARCHAR(50),
+    recipe_name VARCHAR(50),
+    PRIMARY KEY (user_id , recipe_name)
 );
 
-CREATE TABLE UploadedRecipes (
-    userId VARCHAR(50),
-    recipeName VARCHAR(50),
-    PRIMARY KEY (userId , recipeName)
+CREATE TABLE uploaded_recipes (
+    user_id VARCHAR(50),
+    recipe_name VARCHAR(50),
+    PRIMARY KEY (user_id , recipe_name)
 );
 
-CREATE TABLE UserDiet (
-    userId VARCHAR(50),
+CREATE TABLE user_diets (
+    user_id VARCHAR(50),
     diet VARCHAR(50),
-    PRIMARY KEY (userId , diet)
+    PRIMARY KEY (user_id , diet)
 );
 
-CREATE TABLE UserIngredientList (
-    userID VARCHAR(50),
-    ingredientName VARCHAR(50),
+CREATE TABLE user_ingredient_lists (
+    user_id VARCHAR(50),
+    ingredient_name VARCHAR(50),
     amount VARCHAR(50),
-    PRIMARY KEY (userID , ingredientName)
+    PRIMARY KEY (user_id , ingredient_name)
 );
 
-CREATE TABLE GuestIngredientList (
-    sessionID VARCHAR(50),
-    ingredientName VARCHAR(50),
+CREATE TABLE guest_ingredient_lists (
+    session_id VARCHAR(50),
+    ingredient_name VARCHAR(50),
     amount VARCHAR(50),
-    PRIMARY KEY (sessionID , ingredientName)
+    PRIMARY KEY (session_id , ingredient_name)
 );
